d5798d97510fdf56585b7e5a037bac32a25c517a
Rajan Walia <rajanwalia4@gmail.com>
2024-08-14 15:22:24 +0530

    added the file size limit to 50 mb

--- src/api/helpers/multer.helper.js
+++ src/api/helpers/multer.helper.js
@@ -2,14 +2,24 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 50 * 1000000 },// 50 MB = 50*1000000 bytes
+}); 
 const cryptoHandler = require("./crypto-handler.js");
 const fileHandler = require("./file-handler.helper.js");
 
 const uploadAndEncryptFile = async (req, res, next) => {
   upload.single('documentFile')(req, res, async (err) => {
     if (err) {
-      return res.status(500).send({success: false, message : 'Please upload the file in documentFile key in form multipart'});
+        if(err.code == "LIMIT_FILE_SIZE"){
+          return res.status(400).send({success: false, message : "You can maximum upload 50MB file."});
+        }else if(err.code == "LIMIT_UNEXPECTED_FILE"){
+          return res.status(400).send({success: false, message : 'Please upload the file in documentFile key in form/multipart'});
+        }else{
+          console.error("Error while uploadAndEncryptFile", err.message);
+          return res.status(500).send({success: false, message : "something went wrong while uploading document, Please try again!"});
+        }
     }
 
     if (!req.file) {
